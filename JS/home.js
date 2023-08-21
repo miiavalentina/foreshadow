e048b7ba4d3b6804428b0a723ee153fda738e9f2
Valentina Mettler Moreira <valentina_mettlermoreira@sluz.ch>
2023-08-21 20:04:37 +0200

    saved changes

--- JS/home.js
+++ JS/home.js
@@ -1,36 +1,4 @@
-const dropdowns = document.querySelectorAll('.library-dropdown');
-
-dropdowns.forEach(dropdown => {
-
-    //get inner elements from each dropdown
-    const select = dropdown.querySelector('.select');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelector('.menu li');
-    const selected = dropdown.querySelector('.selected');
-
-    //add a click event to the selected element
-    select.addEventListener('click', () => {
-
-        //add the clicked select styles to the selected element
-        select.classList.toggle('select-clicked');
-
-        //add the open styles to the menu element
-        menu.classList.toggle('menu-open');
-
-        //loop through all option elements
-        options.forEach(option => {
-            //add a click event to the option element
-            option.addEventListener('click', () => {
-                //change selected inner text to clicked option inner text
-                selected.innerText = option.innerText;
-                //remove the clicked select styles from the select element
-                select.classList.remove('select-clicked');
-                //remove the open styles from the menu element
-                menu.classList.remove('menu-open');
-            });
-
-            //add active class to clicked option element
-            option.classList.add('active');
-        });
-    });
-});
+function toggleMenu() {
+  document.getElementById("dropdown-menu").classList.toggle("show");
+  document.getElementById("dropdown-button").classList.toggle("active");
+}
